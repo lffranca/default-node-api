05d7b28dfffaac216199b49afb1f04aca0bda405
Lucas França <lucas@localhost.localdomain>
2017-08-14 14:45:40 -0300

    remove build/

--- build/modules/api/auth/services/login.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var verifyData = exports.verifyData = function verifyData(data) {
-  return data;
-};
-//# sourceMappingURL=login.js.map
